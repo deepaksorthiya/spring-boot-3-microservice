6f5d953f63a6a81f376939890aaa07ea6d6aaebe
Deepak Katariya <deepak.sorthiya@gmail.com>
2023-12-30 21:25:33 +0530

    common pom for services

--- spring-boot-microservices/service-one/src/main/java/com/example/ServiceOneApplication.java
+++ spring-boot-microservices/service-one/src/main/java/com/example/ServiceOneApplication.java
@@ -1,4 +1,4 @@
-package com.okta.dev.serviceone;
+package com.example;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
--- spring-boot-microservices/service-one/src/test/java/com/example/ServiceOneApplicationTests.java
+++ spring-boot-microservices/service-one/src/test/java/com/example/ServiceOneApplicationTests.java
@@ -1,4 +1,4 @@
-package com.okta.dev.serviceone;
+package com.example;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
