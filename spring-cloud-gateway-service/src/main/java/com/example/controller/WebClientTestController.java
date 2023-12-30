7099139feb30286e93024eaa1e2d64737cfa0e42
Deepak Katariya <deepak.sorthiya@gmail.com>
2023-12-30 21:55:31 +0530

    Resttemplate support added updated README.md postman collection added

--- spring-cloud-gateway-service/src/main/java/com/example/controller/WebClientTestController.java
+++ spring-cloud-gateway-service/src/main/java/com/example/controller/WebClientTestController.java
@@ -2,9 +2,11 @@ package com.example.controller;
 
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
@@ -14,10 +16,12 @@ import java.util.List;
 public class WebClientTestController {
     private final WebClient.Builder webClient;
     private final DiscoveryClient discoveryClient;
+    private final RestTemplate restTemplate;
 
-    public WebClientTestController(WebClient.Builder webClient, DiscoveryClient discoveryClient) {
+    public WebClientTestController(WebClient.Builder webClient, DiscoveryClient discoveryClient, RestTemplate restTemplate) {
         this.webClient = webClient;
         this.discoveryClient = discoveryClient;
+        this.restTemplate = restTemplate;
     }
 
     @GetMapping("/service-instances/{applicationName}")
@@ -32,8 +36,14 @@ public class WebClientTestController {
     }
 
     @GetMapping("/webclient/test")
-    public Mono<String> getServiceOneMessage() {
+    public Mono<String> getServiceOneMessageUsingWebClient() {
         return webClient.build().get().uri("lb://SERVICE-ONE/secure").retrieve().bodyToMono(String.class);
+    }
+
+    @GetMapping("/resttemplate/test")
+    public ResponseEntity<String> getServiceOneMessageUsingRestTemplate() {
+        ResponseEntity<String> response = restTemplate.getForEntity("lb://SERVICE-ONE/secure", String.class);
+        return response;
     }
 }
 
