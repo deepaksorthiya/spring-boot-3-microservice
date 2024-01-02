4488588ed63cf343a5d402c9ff8e204cce096170
Deepak Katariya <deepak.sorthiya@gmail.com>
2024-01-02 21:31:11 +0530

    Resttemplate support added

--- spring-cloud-gateway-service/src/main/java/com/example/SpringCloudGatewayApplication.java
+++ spring-cloud-gateway-service/src/main/java/com/example/SpringCloudGatewayApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @EnableDiscoveryClient
@@ -19,5 +20,11 @@ public class SpringCloudGatewayApplication {
     @LoadBalanced
     public WebClient.Builder loadBalancedWebClientBuilder() {
         return WebClient.builder();
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate loadBalancedRestTemplate() {
+        return new RestTemplate();
     }
 }
